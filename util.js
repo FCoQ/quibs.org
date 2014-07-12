d022d7ff1bb8cba7b8349d6f01c60be21e76f1e8
qshane <csgomakesmemad@gmail.com>
2014-07-12 10:48:11 -0600

    Fixed time intervals.

--- util.js
+++ util.js
@@ -78,26 +78,40 @@ exports.timeSince = function(timestamp) {
 
     var interval = Math.floor(seconds / 31536000);
 
-    if (interval >= 1) {
+    if (interval > 1) {
         return interval + " years ago";
+    } else if (interval == 1) {
+    	return interval + " year ago";
     }
     interval = Math.floor(seconds / 2592000);
-    if (interval >= 1) {
+    if (interval > 1) {
         return interval + " months ago";
+    } else if (interval == 1) {
+    	return interval + " month ago";
     }
     interval = Math.floor(seconds / 86400);
-    if (interval >= 1) {
+    if (interval > 1) {
         return interval + " days ago";
+    } else if (interval == 1) {
+    	return interval + " day ago";
     }
     interval = Math.floor(seconds / 3600);
-    if (interval >= 1) {
+    if (interval > 1) {
         return interval + " hours ago";
+    } else if (interval == 1) {
+    	return interval + " hour ago";
     }
     interval = Math.floor(seconds / 60);
-    if (interval >= 1) {
+    if (interval > 1) {
         return interval + " minutes ago";
+    } else if (interval == 1) {
+    	return interval + " minute ago";
     }
-    return Math.floor(seconds) + " seconds ago";
+    if (Math.floor(seconds) == 1) {
+    	return Math.floor(seconds) + " second ago";
+    } else {
+    	return Math.floor(seconds) + " seconds ago";
+    }
 }
 
 exports.isset = function(param) {
